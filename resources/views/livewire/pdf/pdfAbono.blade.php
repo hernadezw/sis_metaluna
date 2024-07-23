0573a1f715678b3386aa93f278ab8884c908dba4
Franky Mejia <fryk88@gmail.com>
2024-07-23 15:50:18 -0600

    fix npm run pdf

---
+++ resources/views/livewire/pdf/pdfAbono.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE  html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>fd251001-618d-469f-952a-1511aa2990df</title>
+<meta name="author" content="Franky Emmanuel Mejía Vicente"/>
+<style type="text/css"> * {margin:0; padding:0; text-indent:0; }
+ h1 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 16pt; }
+ p { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 9pt; margin:0pt; }
+ a { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: underline; font-size: 9pt; }
+ .s1 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 7pt; }
+ .s2 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 11pt; }
+ .s3 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 11pt; }
+ .s4 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 10pt; }
+ .s5 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 11pt; }
+ .s6 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 10pt; }
+ table, tbody {vertical-align: top; overflow: visible; }
+</style>
+</head>
+<body>
+<h1 style="padding-top: 2pt;padding-left: 262pt;text-indent: 0pt;text-align: center;">DISTRIBUIDORA METALUNA</h1>
+<p style="padding-left: 262pt;text-indent: 0pt;text-align: center;">9 calle Zona 3, Totonicapan</p>
+<p style="padding-left: 322pt;text-indent: 0pt;text-align: center;">Pedidos a Oficina: Cel: 77664092 - Cel: 30239120 Cel: 46427166 - Cel: 30597733</p>
+<p style="padding-left: 262pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
+<a href="mailto:metaluna@gmail.com" style=" color: black; font-family:&quot;Times New Roman&quot;, serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 9pt;" target="_blank">Correo Electrónico: </a>
+<a href="mailto:metaluna@gmail.com" target="_blank">metaluna@gmail.com</a>
+</p>
+<p class="s1" style="padding-left: 262pt;text-indent: 0pt;line-height: 8pt;text-align: center;">Usuario: Franky Mejia Fecha Impresión: 01/01/2024 12:00 am</p>
+<p style="text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+<table style="border-collapse:collapse;margin-left:5.93pt" cellspacing="0">
+    <tr style="height:13pt">
+        <td style="width:570 pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="6" bgcolor="#F9E1D4">
+            <p class="s2" style="padding-right: 4pt;text-indent: 0pt;line-height: 12pt;text-align: center;">ABONO:</p>
+        </td>
+    </tr>
+    <tr style="height:13pt">
+        <td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="2" bgcolor="#F9E1D4">
+        <p class="s2" style="text-indent: 0pt;line-height: 12pt;text-align: center;">VENTA:</p>
+        </td>
+
+
+    <td style="width:84pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+    <p class="s3" style="padding-left: 13pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Fecha Venta:</p>
+    </td>
+    <td style="width:117pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-left: 11pt;text-indent: 0pt;line-height: 12pt;text-align: left;">{{$venta['fecha_venta']}}</p>
+    </td>
+    <td style="width:72pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+    <p class="s3" style="padding-left: 5pt;padding-right: 5pt;text-indent: 0pt;line-height: 12pt;text-align: center;">No. Venta</p>
+    </td>
+    <td style="width:12pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">{{$venta['no_venta']}}</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+    <td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+    <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Tipo Pago:</p>
+    </td>
+    <td style="width:111pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-left: 25pt;text-indent: 0pt;line-height: 12pt;text-align: left;">{{$venta['forma_pago']}}</p>
+    </td>
+    <td style="width:84pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+    <p class="s3" style="padding-left: 13pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Envio:</p>
+    </td>
+    <td style="width:117pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-left: 11pt;text-indent: 0pt;line-height: 12pt;text-align: left;"></p>
+    </td>
+    <td style="width:72pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+    <p class="s3" style="padding-left: 5pt;padding-right: 5pt;text-indent: 0pt;line-height: 12pt;text-align: center;">Pagina</p>
+    </td>
+    <td style="width:12pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">1 de 1</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+<td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+<p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Código:</p>
+</td>
+<td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+<p class="s4" style="text-indent: 0pt;text-align: center;">{{$cliente['codigo_mayorista']}}</p>
+</td>
+<td style="width:63pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+<p class="s3" style="padding-left: 10pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Telefono:</p>
+</td>
+<td style="width:78pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+<p class="s4" style="padding-left: 18pt;text-indent: 0pt;text-align: left;">{{$cliente['telefono_principal']}}</p>
+</td>
+<td style="width:70pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+<p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Correo elec.:</p>
+</td>
+<td style="width:103pt;border-top-style:solid;border-top-width:1pt; border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+<p style="padding-left: 10pt;text-indent: 0pt;line-height: 12pt;text-align: left;">
+<a href="mailto:pedro@gmail.com" class="s5">{{$cliente['correo_electronico']}}</a>
+</p>
+</td>
+</tr>
+<tr style="height:13pt">
+<td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+<p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Empresa:</p>
+</td>
+<td style="width:402pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="5">
+<p class="s3" style="padding-left: 67pt;text-indent: 0pt;line-height: 12pt;text-align: left;">{{$cliente['nombre_empresa']}}</p>
+</td>
+
+</tr>
+<tr style="height:13pt">
+<td style="width:71pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+<p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Dirección:</p>
+</td>
+<td style="width:465pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="5">
+<p class="s3" style="padding-left: 5pt;text-indent: 0pt;line-height: 12pt;text-align: left;">{{$cliente['direccion_fisica']}}</p>
+</td>
+</tr>
+</table>
+<p style="padding-top: 2pt;text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+
+
+
+<table style="border-collapse:collapse;margin-left:5.93pt" cellspacing="0">
+<tr style="height:13pt">
+    <td style="width:570 pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="6" bgcolor="#F9E1D4">
+        <p class="s2" style="padding-right: 4pt;text-indent: 0pt;line-height: 12pt;text-align: center;">DETALLE:</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">No. Abono</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">Fecha Abono</p>
+    </td>
+    <td style="width:95pt;bord  er-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="text-indent: 0pt;line-height: 12pt;text-align: center;">No Venta   </p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="padding-left: 5pt;padding-right: 5pt;text-indent: 0pt;line-height: 12pt;text-align: center;">Saldo credito</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="padding-left: 5pt;padding-right: 5pt;text-indent: 0pt;line-height: 12pt;text-align: center;">Total Abono</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#F9E1D4">
+        <p class="s3" style="padding-left: 27pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Nuevo saldo</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="text-indent: 0pt;text-align: center;">{{$abono['no_abono']}}</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="text-indent: 0pt;text-align: center;">{{$abono['fecha_abono']}}</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-left: 5pt;text-indent: 0pt;text-align: center;">{{$abono['venta_id']}}</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p class="s3" style="text-indent: 0pt;text-align: center;">{{$abono['saldo_credito']}}</p>
+        </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-right: 4pt;text-indent: 0pt;text-align: center;">Q. {{$abono['total_abono']}}</p>
+    </td>
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p class="s3" style="padding-right: 4pt;text-indent: 0pt;text-align: center;">Q. {{$abono['total_saldo']}}</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+    <td style="width:570 pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="6" >
+        <p class="s3" style="padding-right: 4pt;text-indent: 0pt;line-height: 12pt;text-align: star;">Observaciones: {{$abono['observaciones']}}</p>
+    </td>
+</tr>
+
+<tr style="height:13pt">
+    <td style="width:95pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"colspan="3">
+    <p class="s3" style="text-indent: 0pt;text-align: center;">{{$abono['tipo_pago']}}</p>
+    </td>
+    <td style="width:570 pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="3" >
+        <p class="s3" style="padding-right: 4pt;text-indent: 0pt;line-height: 12pt;text-align: star;">No Operacion: {{$abono['no_abono']}}</p>
+    </td>
+</tr>
+<tr style="height:13pt">
+    <td style="width:570 pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" colspan="6" >
+        <p class="s3" style="padding-right: 4pt;text-indent: 0pt;line-height: 12pt;text-align: star;">Detalle Pago: {{$abono['detalle_pago']}}</p>
+    </td>
+</tr>
+
+
+
+
+</table>
+<p style="padding-top: 1pt;text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+
+
+</body>
+</html>
---
+++ resources/views/livewire/pdf/pdfEstadoCuentaVertical.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE  html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>d9fca54a-9688-4786-9024-5e00131dad85</title>
+<meta name="author" content="Luis Antonio Hernández Tzoc"/>
+<style type="text/css"> * {margin:0; padding:0; text-indent:0; }
+ .s1 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 11pt; }
+ .s2 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 8pt; }
+ .s3 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 10pt; }
+ .s4 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: bold; text-decoration: none; font-size: 10pt; }
+ p { color: black; font-family:Calibri, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 11pt; margin:0pt; }
+ .s5 { color: black; font-family:"Times New Roman", serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 11pt; }
+ table, tbody {vertical-align: top; overflow: visible; }
+</style>
+</head>
+<body>
+<table style="border-collapse:collapse;margin-left:298.91pt" cellspacing="0">
+<tr style="height:20pt">
+<td style="width:87pt">
+<p style="text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+</td>
+<td style="width:198pt">
+<p class="s1" style="padding-left: 73pt;text-indent: 0pt;line-height: 12pt;text-align: left;">REPORTE DE SALDOS</p>
+</td>
+<td style="width:121pt">
+<p style="text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+</td>
+</tr>
+<tr style="height:17pt">
+<td style="width:87pt">
+<p class="s2" style="padding-top: 7pt;padding-left: 2pt;text-indent: 0pt;line-height: 8pt;text-align: left;">Usuario:</p>
+</td>
+<td style="width:198pt">
+<p class="s2" style="padding-top: 7pt;padding-left: 56pt;text-indent: 0pt;line-height: 8pt;text-align: left;">Fecha de Impresión:</p>
+</td>
+<td style="width:121pt">
+<p class="s2" style="padding-top: 7pt;padding-left: 75pt;text-indent: 0pt;line-height: 8pt;text-align: left;">Página 1 de 1</p>
+</td>
+</tr>
+</table>
+<p style="text-indent: 0pt;text-align: left;">
+<br/>
+</p>
+<table style="border-collapse:collapse;margin-left:5.33pt" cellspacing="0">
+<tr style="height:13pt">
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+<p class="s1" style="padding-left: 44pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Codigo Cliente</p>
+</td>
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+<p class="s1" style="padding-left: 36pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Nombre de cliente</p>
+</td>
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+<p class="s1" style="padding-left: 1pt;text-indent: 0pt;line-height: 12pt;text-align: center;">Total Abono</p>
+</td>
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+<p class="s1" style="padding-left: 8pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Total Credito</p>
+</td>
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+    <p class="s1" style="padding-left: 8pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Dias Credito</p>
+    </td>
+<td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt" bgcolor="#FAE3D4">
+<p class="s1" style="padding-left: 26pt;text-indent: 0pt;line-height: 12pt;text-align: left;">Saldo</p>
+</td>
+</tr>
+
+@foreach ($estado_cuenta as $item)
+<tr style="height:13pt">
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+    <p style="text-indent: 0pt;text-align: left;">{{$item['cliente']['codigo']}}</p>
+    </td>
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p style="text-indent: 0pt;text-align: left;">{{$item['cliente']['nombre_empresa']}}</p>
+    </td>
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p style="text-indent: 0pt;text-align: left;">Q. {{$item['total_abono']}}</p>
+    </td>
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p style="text-indent: 0pt;text-align: left;">Q. {{$item['total_credito']}}</p>
+    </td>
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p style="text-indent: 0pt;text-align: left;">{{$item['cliente']['dias_limite_credito']}} Dias</p>
+    </td>
+    <td style="width:140pt;border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+        <p style="text-indent: 0pt;text-align: left;">Q. {{$item['total_credito']-$item['total_abono']}}</p>
+    </td>
+
+</tr>
+
+@endforeach
+</table>
+
+
+</body>
+</html>
